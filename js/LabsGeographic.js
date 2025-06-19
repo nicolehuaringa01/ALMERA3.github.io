70885ad3b04a4894317e502923b0f3e708680ed3
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-06-19 17:01:02 +0200

    Update LabsGeographic.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- js/LabsGeographic.js
+++ js/LabsGeographic.js
@@ -1,4 +1,4 @@
-const csvFilePath = "observable2020SurveyUpdatedData2025.csv"; 
+const csvFilePath = "observable2020SurveyUpdatedData.csv"; 
 
 console.log("Attempting to load CSV from:", csvFilePath);
 
