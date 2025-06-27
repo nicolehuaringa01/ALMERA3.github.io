7809f8df17462e4114ae8e3b4331dbc90c17ba08
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-06-27 11:56:40 +0200

    Update 6.5International_PT.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- js/6.Quality_Management_and_Reporting/6.5International_PT.js
+++ js/6.Quality_Management_and_Reporting/6.5International_PT.js
@@ -88,7 +88,7 @@ async function initializeInternational_PTChart() {
                 axis: false // Hide y-axis as it's a single bar
             },
             x: {
-                label: "Participation in Development of Analytical Methods ",
+                label: "Participation in International Proficiency Tests ",
                 labelAnchor: "center",
                 labelOffset: 40, // Space for the label
                 domain: [0, 1] // Ensure x-axis spans 0 to 1 for percentages
