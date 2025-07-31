a4398fe3a491cdd7f68792f385e82a1a59d829c6
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-07-31 14:00:41 +0200

    Update load_navigation.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- Main/components/Navigation/load_navigation.js
+++ Main/components/Navigation/load_navigation.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navPlaceholder = document.getElementById('main-nav-placeholder');
 
     if (navPlaceholder) {
-        fetch('../js/Update_Shortcuts/navbar.html')
+        fetch('navbar.html')
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
