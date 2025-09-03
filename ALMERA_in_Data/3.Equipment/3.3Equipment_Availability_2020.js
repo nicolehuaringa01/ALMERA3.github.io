92f00c2744c57956ac1a57cc928c4d1b6a93dd4e
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-09-03 14:06:11 +0200

    Update and rename 3.3Equipmment_Availability.js to 3.3Equipment_Availability_2020.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/3.Equipment/3.3Equipment_Availability_2020.js
+++ ALMERA_in_Data/3.Equipment/3.3Equipment_Availability_2020.js
@@ -1,4 +1,4 @@
-// ALMERA_in_Data/3.Equipment/3.3Equipmment_Availability_2020.js
+// ALMERA_in_Data/3.Equipment/3.3Equipment_Availability_2020.js
 
 // Global variables to store processed data and selected state
 let allSurveyData; // Will hold the loaded CSV data
