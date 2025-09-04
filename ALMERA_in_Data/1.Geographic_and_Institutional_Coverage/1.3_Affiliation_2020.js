9afc31e755da8c3c4067c35103cbea19d5013e80
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-09-04 14:44:14 +0200

    Update 1.3_Affiliation_2020.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/1.Geographic_and_Institutional_Coverage/1.3_Affiliation_2020.js
+++ ALMERA_in_Data/1.Geographic_and_Institutional_Coverage/1.3_Affiliation_2020.js
@@ -1,5 +1,5 @@
 // ALMERA3.github.io/ALMERA_in_Data/1.Geographic_and_Institutional_Coverage/1.3Affiliation.js
-const csvDataPath3 = "/ALMERA3.github.io/data/Observable2020Survey.csv"; // User-provided path
+const csvDataPath3 = "/ALMERA3.github.io/data/2020_ALMERA_Capabilities_Survey.csv"; // User-provided path
 
 // This function processes the raw data to count affiliations
 function getAffiliationCounts(data, affiliationColumn) {
