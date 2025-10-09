7cbc1b040ed95e57f2f71c539cb546a11830f848
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 15:27:42 +0200

    Update and rename ALMERA_in_Data/6.Quality_Management_and_Reporting/6.15Report_Data_to_IAEA.js to ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.15Report_Data_to_IAEA.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.15Report_Data_to_IAEA.js
+++ ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.15Report_Data_to_IAEA.js
@@ -1,4 +1,4 @@
-// ALMERA_in_Data/6.Quality_Management_and_Reporting/6.15Report_Data_to_IAEA.js
+// ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.15Report_Data_to_IAEA.js
 
 const csvDataPath15 = "/ALMERA3.github.io/data/2020_ALMERA_Capabilities_Survey.csv";
 
