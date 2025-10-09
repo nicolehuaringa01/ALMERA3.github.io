6b1fb17b642a80e382feef3363bc883bbaca1576
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 11:37:15 +0200

    Update and rename ALMERA_in_Data/2.Human_Resources_and_Training/2.8HostALMERACMs_2020.js to ALMERA_in_Data/2020/2.Human_Resources_and_Training/2.8HostALMERACMs_2020.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2020/2.Human_Resources_and_Training/2.8HostALMERACMs_2020.js
+++ ALMERA_in_Data/2020/2.Human_Resources_and_Training/2.8HostALMERACMs_2020.js
@@ -1,4 +1,4 @@
-// ALMERA_in_Data/2.Human_Resources_and_Training/2.8HostALMERACMs_2020.js
+// ALMERA_in_Data/2020/2.Human_Resources_and_Training/2.8HostALMERACMs_2020.js
 
 const csvDataPath8 = "/ALMERA3.github.io/data/2020_ALMERA_Capabilities_Survey.csv";
 
