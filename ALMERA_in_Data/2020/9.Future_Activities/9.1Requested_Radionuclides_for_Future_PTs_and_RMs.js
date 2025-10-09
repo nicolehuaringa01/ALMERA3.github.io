626db38ed014cd269244e48b6b787baabfd60ded
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 15:40:37 +0200

    Update and rename ALMERA_in_Data/9.Future_Activities/9.1Requested_Radionuclides_for_Future_PTs_and_RMs.js to ALMERA_in_Data/2020/9.Future_Activities/9.1Requested_Radionuclides_for_Future_PTs_and_RMs.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2020/9.Future_Activities/9.1Requested_Radionuclides_for_Future_PTs_and_RMs.js
+++ ALMERA_in_Data/2020/9.Future_Activities/9.1Requested_Radionuclides_for_Future_PTs_and_RMs.js
@@ -1,6 +1,6 @@
-// ALMERA_in_Data/9.Future_Activities/9.1Requested_Radionuclides_for_Future_PTs_and_RMs.js
+// ALMERA_in_Data/2020/9.Future_Activities/9.1Requested_Radionuclides_for_Future_PTs_and_RMs.js
 
-const csvDataPath1 = "/ALMERA3.github.io/data/Observable2020Survey.csv"; // Consistent CSV path
+const csvDataPath1 = "/ALMERA3.github.io/data/2020_ALMERA_Capabilities_Survey.csv"; // Consistent CSV path
 const topojsonPath1 = "https://cdn.jsdelivr.net/npm/world-atlas@2/land-50m.json";
 
 // Declare variables that will hold our processed data and state
