3aaddb6056002c153aa9e559f2e8d53ffb0dae71
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 12:01:39 +0200

    Update and rename ALMERA_in_Data/1.Geographic_and_Institutional_Coverage/1.3_Affiliation_2025.js to ALMERA_in_Data/2025/1.Geographic_and_Institutional_Coverage/1.3_Affiliation_2025.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2025/1.Geographic_and_Institutional_Coverage/1.3_Affiliation_2025.js
+++ ALMERA_in_Data/2025/1.Geographic_and_Institutional_Coverage/1.3_Affiliation_2025.js
@@ -1,4 +1,4 @@
-// ALMERA3.github.io/ALMERA_in_Data/1.Geographic_and_Institutional_Coverage/1.3_Affiliation_2025.js
+// ALMERA_in_Data/2025/1.Geographic_and_Institutional_Coverage/1.3_Affiliation_2025.js
 const csvDataPath3 = "/ALMERA3.github.io/data/2025_ALMERA_Capabilities_Survey.csv"; // User-provided path
 
 // This function processes the raw data to count affiliations
