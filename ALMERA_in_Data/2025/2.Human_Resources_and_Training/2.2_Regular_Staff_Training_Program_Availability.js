dc39b7e768fc5e41bf3f4542bf1b7c60b3d3d061
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 12:10:52 +0200

    Update 2.2_Regular_Staff_Training_Program_Availability.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2025/2.Human_Resources_and_Training/2.2_Regular_Staff_Training_Program_Availability.js
+++ ALMERA_in_Data/2025/2.Human_Resources_and_Training/2.2_Regular_Staff_Training_Program_Availability.js
@@ -1,6 +1,6 @@
 // ALMERA_in_Data/2025/2.Human_Resources_and_Training/2.2_Regular_Staff_Training_Program_Availability.js
 
-const csvDataPath2 = "/ALMERA3.github.io/data/Observable2020Survey.csv"; 
+const csvDataPath2 = "/ALMERA3.github.io/data/2025_ALMERA_Capabilities_Survey.csv"; 
 
 async function initializeStaffTrainingChart() {
     const container = document.getElementById("staff-training-chart-container");
