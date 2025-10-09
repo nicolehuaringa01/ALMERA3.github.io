c15615f8798f6830463c1b58a1e3c0166c5f5c64
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 15:36:26 +0200

    Update and rename ALMERA_in_Data/7.EnvironmentalMonitoringCapabilities/7.6Gamma_Dose_Rate_Monitoring_Network_Availability.js to ALMERA_in_Data/2020/7.EnvironmentalMonitoringCapabilities/7.6Gamma_Dose_Rate_Monitoring_Network_Availability.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2020/7.EnvironmentalMonitoringCapabilities/7.6Gamma_Dose_Rate_Monitoring_Network_Availability.js
+++ ALMERA_in_Data/2020/7.EnvironmentalMonitoringCapabilities/7.6Gamma_Dose_Rate_Monitoring_Network_Availability.js
@@ -1,6 +1,6 @@
-// ALMERA_in_Data/7.EnvironmentalMonitoringCapabilities/7.6Gamma_Dose_Rate_Monitoring_Network_Availability
+// ALMERA_in_Data/2020/7.EnvironmentalMonitoringCapabilities/7.6Gamma_Dose_Rate_Monitoring_Network_Availability.js
 
-const csvDataPath6 = "/ALMERA3.github.io/data/Observable2020Survey.csv"; // Using 'csvDataPath' for clarity in this file
+const csvDataPath6 = "/ALMERA3.github.io/data/2020_ALMERA_Capabilities_Survey.csv";
 
 async function initializeGamma_Dose_Rate_Monitoring_Network_AvailabilityChart() {
     const container = document.getElementById("Gamma_Dose_Rate_Monitoring_Network_Availability-chart-container");
