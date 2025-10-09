89c1940d4b80fcc7d3caa3b7d0dae893e36e2137
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 15:32:14 +0200

    Update and rename ALMERA_in_Data/6.Quality_Management_and_Reporting/6.9Decay_Library.js to ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.9Decay_Library.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.9Decay_Library.js
+++ ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.9Decay_Library.js
@@ -1,10 +1,6 @@
-// ALMERA_in_Data/6.Quality_Management_and_Reporting/6.9Decay_Library.js
+// ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.9Decay_Library.js
 
-// IMPORTANT: Verify this path carefully!
-// This path is relative to the HTML file that loads this JS.
-// Assuming your CSV is in the 'data' subfolder within your GitHub Pages project's root
-// (e.g., https://nicolehuaringa01.github.io/ALMERA3.github.io/data/Observable2020Survey.csv)
-const csvDataPath9 = "/ALMERA3.github.io/data/Observable2020Survey.csv"; // User-provided path
+const csvDataPath9 = "/ALMERA3.github.io/data/2020_ALMERA_Capabilities_Survey.csv"; // User-provided path
 
 // This function processes the raw data to count Decay_Librarys
 function getDecay_LibraryCounts(data, Decay_LibraryColumn) {
