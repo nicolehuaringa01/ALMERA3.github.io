99995e2b362155d6c5dd1ca2e151f9e2cb65a9bc
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 12:13:50 +0200

    Update and rename ALMERA_in_Data/2.Human_Resources_and_Training/2.5AbleToHostTrainingCourses.js to ALMERA_in_Data/2025/2.Human_Resources_and_Training/2.5_Able_To_Host_Training_Courses.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2025/2.Human_Resources_and_Training/2.5_Able_To_Host_Training_Courses.js
+++ ALMERA_in_Data/2025/2.Human_Resources_and_Training/2.5_Able_To_Host_Training_Courses.js
@@ -1,4 +1,4 @@
-// ALMERA_in_Data/2.Human_Resources_and_Training/2.5AbleToHostTrainingCourses.js
+// ALMERA_in_Data/2025/2.Human_Resources_and_Training/2.5_Able_To_Host_Training_Courses.js
 
 const csvDataPath5 = "/ALMERA3.github.io/data/2025_ALMERA_Capabilities_Survey.csv";
 
