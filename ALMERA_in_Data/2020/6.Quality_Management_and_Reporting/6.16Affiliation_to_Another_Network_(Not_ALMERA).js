e700d13abee2cd52f9b6df249adf82468cc25b0a
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-09 15:28:13 +0200

    Update and rename ALMERA_in_Data/6.Quality_Management_and_Reporting/6.16Affiliation_to_Another_Network_(Not_ALMERA).js to ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.16Affiliation_to_Another_Network_(Not_ALMERA).js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.16Affiliation_to_Another_Network_(Not_ALMERA).js
+++ ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.16Affiliation_to_Another_Network_(Not_ALMERA).js
@@ -1,4 +1,4 @@
-// ALMERA_in_Data/6.Quality_Management_and_Reporting/6.16Affiliation_to_Another_Network.js
+// ALMERA_in_Data/2020/6.Quality_Management_and_Reporting/6.16Affiliation_to_Another_Network.js
 
 const csvDataPath16 = "/ALMERA3.github.io/data/2020_ALMERA_Capabilities_Survey.csv";
 
