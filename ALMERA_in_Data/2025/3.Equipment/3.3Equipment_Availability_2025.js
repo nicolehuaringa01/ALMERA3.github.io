f625e236ecf1fbd9deec106fef00abeed594555f
nicolehuaringa01 <nicolehuaringa01@gmail.com>
2025-10-10 13:44:03 +0200

    Update 3.3Equipment_Availability_2025.js
    Signed-off-by: nicolehuaringa01 <nicolehuaringa01@gmail.com>

--- ALMERA_in_Data/2025/3.Equipment/3.3Equipment_Availability_2025.js
+++ ALMERA_in_Data/2025/3.Equipment/3.3Equipment_Availability_2025.js
@@ -425,7 +425,7 @@ const updateLabInfoDisplay = () => {
 };
 
 // --- Data Loading and Initialization ---
-d3.csv("/ALMERA3.github.io/data/Survey_of_ALMERA_Laboratories'_2025-09-03_08_56_07.csv").then(data => {
+d3.csv("/ALMERA3.github.io/data/2025_ALMERA_Capabilities_Survey.csv").then(data => {
     allSurveyData = data; // Assign loaded data to the global variable
 
     // Process data using the helper functions
